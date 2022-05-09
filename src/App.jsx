4dc24d14b70a829456a2b0b7d61858f0b484c45d
Nandom <nandommamdam@gmail.com>
2022-05-09 18:19:02 +0100

    added sessionStorage for user login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ import { ReactComponent as ProfileIcon } from './icons/profile-circle.svg'
 import { ReactComponent as UserIcon } from './icons/profile.svg'
 
 function App() {
-	const [user, setUser] = React.useState('nandom')
+	const [user, setUser] = React.useState(
+		sessionStorage.getItem('username') || null
+	)
+
+	const handleLogout = () => {
+		sessionStorage.setItem('username', null)
+		setUser(null)
+	}
+
+	const handleLogin = (event) => {
+		event.preventDefault()
+		const username = event.target.username.value
+		sessionStorage.setItem('username', username)
+		setUser(username)
+	}
 
 	return (
 		<div>
@@ -15,7 +29,9 @@ function App() {
 							<ProfileIcon className="profile__icon" />
 							<p>{user}</p>
 						</div>
-						<button className="button button-secondary">Logout</button>
+						<button className="button button-secondary" onClick={handleLogout}>
+							Logout
+						</button>
 					</header>
 
 					<ul className="list">
@@ -53,12 +69,17 @@ function App() {
 			) : (
 				<div className="card">
 					<h3 className="card__title">Login</h3>
-					<form className="form">
+					<form className="form" onSubmit={handleLogin}>
 						<div className="form__control">
-							<label className="form__label" for="username">
+							<label className="form__label" htmlFor="username">
 								Username
 							</label>
-							<input className="form__input" id="username" autoComplete="off" />
+							<input
+								className="form__input"
+								id="username"
+								autoComplete="off"
+								required
+							/>
 						</div>
 
 						<div className="form__control">
