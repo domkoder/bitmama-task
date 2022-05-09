b2f98c7e406f510faf1f3ba3e272e622247ab883
Nandom <nandommamdam@gmail.com>
2022-05-09 16:53:23 +0100

    finished the profile and user ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+import { ReactComponent as ProfileIcon } from './icons/profile-circle.svg'
+import { ReactComponent as UserIcon } from './icons/profile.svg'
 
 function App() {
 	const [user, setUser] = React.useState('nandom')
@@ -9,9 +11,44 @@ function App() {
 			{user ? (
 				<div className="card">
 					<header className="header">
-						<p>{user}</p>
+						<div className="profile__info ">
+							<ProfileIcon className="profile__icon" />
+							<p>{user}</p>
+						</div>
 						<button className="button button-secondary">Logout</button>
 					</header>
+
+					<ul className="list">
+						<li className="list__item">
+							<div className="user__info">
+								<span className="user__icon">
+									<UserIcon />
+								</span>
+								<div>
+									<div>
+										levi <span className="user__status active">active</span>
+									</div>
+									<div className="user__tab">Tab 1</div>
+								</div>
+							</div>
+							<button class="delete">×</button>
+						</li>
+
+						<li className="list__item">
+							<div className="user__info">
+								<span className="user__icon">
+									<UserIcon />
+								</span>
+								<div className="user__container">
+									<div>
+										Robinson <span className="user__status">idle</span>
+									</div>
+									<div className="user__tab">Tab 1</div>
+								</div>
+							</div>
+							<button class="delete">×</button>
+						</li>
+					</ul>
 				</div>
 			) : (
 				<div className="card">
