bb4c1acf23b113bde47f147688b61f1456416f1c
Nandom <nandommamdam@gmail.com>
2022-05-09 04:04:21 +0100

    separated my app in to two defferent sections created the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
+import React from 'react'
 import './App.css'
 
 function App() {
-	return (
-		<div className="card">
-			<h3 className="card__title">Login</h3>
-			<form className="form">
-				<div className="form__control">
-					<label className="form__label" for="username">
-						Username
-					</label>
-					<input className="form__input" id="username" autoComplete="off" />
-				</div>
+	const [user, setUser] = React.useState('nandom')
 
-				<div className="form__control">
-					<button className="button" type="submit">
-						Login
-					</button>
+	return (
+		<div>
+			{user ? (
+				<div className="card">
+					<header className="header">
+						<p>{user}</p>
+						<button className="button button-secondary">Logout</button>
+					</header>
 				</div>
-			</form>
+			) : (
+				<div className="card">
+					<h3 className="card__title">Login</h3>
+					<form className="form">
+						<div className="form__control">
+							<label className="form__label" for="username">
+								Username
+							</label>
+							<input className="form__input" id="username" autoComplete="off" />
+						</div>
+
+						<div className="form__control">
+							<button className="button" type="submit">
+								Login
+							</button>
+						</div>
+					</form>
+				</div>
+			)}
 		</div>
 	)
 }
